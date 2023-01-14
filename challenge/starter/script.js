f0251b8a175ee4e9035df11e74465fe34e0b96a6
Zak <zakirali1@hotmail.co.uk>
2023-01-14 11:59:54 +0000

    finished code upload

--- challenge/starter/script.js
+++ challenge/starter/script.js
@@ -88,20 +88,86 @@ var upperCasedCharacters = [
   'Z'
 ];
 
+// Global Variables
+
+const passwd = prompt("How many characters do you want to be in your password?");
+
+let lowercase;
+let uppercase;
+let numeric;
+let special;
+
+// Function to start the password generation process, using a Regex pattern to restrict values to be within range of 10-64
+
+const start = () => {
+  const re = /^(1[0-9]|[2-5][0-9]|6[0-4])$/
+
+  if(re.test(passwd)) {
+    getPasswordOptions();
+  } else {
+     alert("must be select between 10-64 characters long");
+     location.reload();
+  }
+}
+
+start();
+
+
 // Function to prompt user for password options
 function getPasswordOptions() {
+
+ lowerCase = confirm("do you want lower case characters?");
+ upperCase = confirm("do you want upper case characters?");
+ numeric = confirm("do you want numeric characters?");
+ special = confirm("do you want special characters?");
+
+ if (lowerCase|| upperCase || numeric || special ) {
+  
+  generatePassword();
+
+ } else {
+
+alert("You need to select at least 1 type of character to generate a password.")
+location.reload();
+ }
 
 }
 
 // Function for getting a random element from an array
 function getRandom(arr) {
 
+  const check = Math.floor(Math.random() * arr.length);
+  
+  return arr[check];
+ 
 }
 
 // Function to generate password with user input
-function generatePassword() {
+ function generatePassword() {
+  let newstring = "";
+  const numericPasswd = Number.parseInt(passwd, 10)
 
+   for(let i = 0; i < numericPasswd; i++)  {
+     if(lowerCase) {
+      newstring += getRandom(lowerCasedCharacters);
+     }
+      if(upperCase) {
+        newstring += getRandom(upperCasedCharacters);
+      }
+      if(numeric) {
+        newstring += getRandom(numericCharacters);
+      }
+      if(special) {
+        newstring += getRandom(specialCharacters);
+      }
+
+       
+   }
+   
+   return newstring.substring(0,numericPasswd);
 }
+
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector('#generate');
@@ -115,4 +181,4 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener('click', writePassword);
+generateBtn.addEventListener('click', writePassword) ;
